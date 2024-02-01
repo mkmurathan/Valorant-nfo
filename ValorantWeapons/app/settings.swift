bf02e6e45a2463422c7b20ab1da5619b9a572a12
Murathan Karagöz <murathankaragooz@hotmail.com>
2024-02-02 02:44:59 +0300

    settings page, splash screen view page

---
+++ ValorantWeapons/app/settings.swift
@@ -0,0 +1,18 @@
+//
+//  settings.swift
+//  ValorantWeapons
+//
+//  Created by Murathan karagöz on 2.02.2024.
+//
+
+import SwiftUI
+
+struct settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settings()
+}
---
+++ ValorantWeapons/app/splashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  splashScreenView.swift
+//  ValorantWeapons
+//
+//  Created by Murathan karagöz on 1.02.2024.
+//
+
+import SwiftUI
+
+struct splashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    splashScreenView()
+}
